9b619a4f8cab465d443442680e63efc8d80a7074
n-schulze13 <108143589+n-schulze13@users.noreply.github.com>
2024-08-19 18:12:20 +0000

    commit 2: part 2 in book

--- week6/arcadeGame.py
+++ week6/arcadeGame.py
@@ -29,5 +29,30 @@ player_image = PhotoImage(file='greenChar.gif')
 # use image to create character
 mychar = canvas.create_image(200, 360, image=player_image)
 
+# make variables and lists
+candy_list = []
+bad_candy_list = []
+candy_speed = 2
+candy_color_list = ['red', 'yellow', 'purple', 'green', 'blue', 'pink', 'white']
+
+# random candy function
+def make_candy():
+    xposition = random.randint(1,400)
+    candy_color = random.choice(candy_list)
+    candy = canvas.create_oval(xposition, 0, xposition+30, fill = candy_color)
+    candy_list.append(candy)
+    if candy_color == 'red':
+        bad_candy_list.append(candy)
+    window.after(1000,make_candy)
+
+# move the candy
+def move_candy():
+    for candy in candy_list:
+        canvas.move(candy, 0, candy_speed)
+        if canvas.coords(candy)[1] > 400:
+            xposition = random.randint(1,400)
+            canvas.coords(candy,xposition, 0, xposition+30, 30)
+    window.after(50, move_candy)
+
 # GUI main event loop
 window.mainloop()
